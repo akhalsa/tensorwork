5bfeddf45c1581ad2ddfe2e1055e631c5c9ee1b6
Avtar Khalsa <akhalsa43@gmail.com>
2016-04-26 09:44:13 -0500

    maybe extract

--- proj1.py
+++ proj1.py
@@ -24,5 +24,34 @@ def maybe_download(filename, expected_bytes, force=False):
       'Failed to verify ' + filename + '. Can you get to it with a browser?')
   return filename
 
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  return data_folders
+
 train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
 test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+num_classes = 10
+np.random.seed(133)
+
+
+  
+train_folders = maybe_extract(train_filename)
+test_folders = maybe_extract(test_filename)
